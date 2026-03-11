fd7dd3499223c5b2237fcf4b7037ee8ffb7c4092
lucja <lucja.carpenter@interia.pl>
2026-03-11 17:35:54 +0100

    fix: GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",  // <--- tutaj dodajemy base
+  base: "/aurine-beauty-ads-vision/",  // <--- tutaj podaj nazwę repo
   server: {
     host: "::",
     port: 8080,
